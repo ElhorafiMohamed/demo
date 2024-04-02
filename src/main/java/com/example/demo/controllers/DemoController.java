d1663cf87ad88a80e04c95af010544e27241da58
Mohamed Elhorafi <96639132+ElhorafiMohamed@users.noreply.github.com>
2024-04-02 16:18:20 +0000

    logger infos

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -2,6 +2,9 @@ package com.example.demo.controllers;
 
 import com.example.demo.services.DemoService;
 import lombok.AllArgsConstructor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private final DemoService demoService;
-
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
 
     @GetMapping("/admin_only")
     public ResponseEntity<String> admin_only() {
@@ -19,6 +22,7 @@ public class DemoController {
 
     @GetMapping("/demo")
     public ResponseEntity<String> demo() {
+        logger.info("Received request to /hello");
         return demoService.infos();
     }
 }
